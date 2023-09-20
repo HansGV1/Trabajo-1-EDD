11a629991b8579a7abbd97608727c97c49aedf86
Hans García <hansgarcia@ibm.com>
2023-09-20 18:12:53 -0500

    Plantilla base para código final
    Por favor agregue su código aquí, recuerde usar ramas o branches de GitHub para no dañar el ORIGINAL ( es decir copie la plantilla y úsela para desarrollar cuando se tenga la funcionalidad final la pegamos aquí)

---
+++ Snake.py
@@ -0,0 +1,28 @@
+# Trabajpo 1 Estructura de Datos
+#
+# Snake Game
+#
+# Integrantes:
+# + Hans Guillermo García Vargas - hggarciag@unal.edu.co
+# + Ingrese aquí su nombre - ingrese aquí su correo
+# + Ingrese aquí su nombre - ingrese aquí su correo
+# + Ingrese aquí su nombre - ingrese aquí su correo
+#
+# Universidad Nacional de Colombia
+#
+#
+
+
+# Ambiente (Interfaz)
+# Ingrese su código aquí
+
+
+
+# Serpiente
+# Ingrese su código aquí
+
+
+
+# Manzana
+# Ingrese su código aquí
+
